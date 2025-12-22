2bacf9fb5730b794ca3ebb175c474b7590c58930
Vitaliy Kondratiev <kondrav@live.ca>
2025-12-21 22:08:48 -0500

    remove codeblock, legacy component

--- playground/src/lessons/components/index.ts
+++ playground/src/lessons/components/index.ts
@@ -1,5 +1,4 @@
 export { default as LessonHeader } from './LessonHeader';
 export { default as Section } from './Section';
 export { default as TakeawayList } from './TakeawayList';
-export { default as CodeBlock } from './CodeBlock';
 export { default as CodeSnippet } from './CodeSnippet';
